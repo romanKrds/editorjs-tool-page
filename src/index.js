175e639f49ec6fe816b8c332f2221e748d4ffbef
romanKrds <roman.kordas7@gmail.com>
2020-08-18 14:01:49 +0300

    rebuild view swap conditions

--- src/index.js
+++ src/index.js
@@ -12,16 +12,14 @@ require('./index.css').toString();
 /**
  * @typedef {object} PageConfig
  * @property {string} placeholder - placeholder for the empty Page
- * @property {string} wikiPageEndpoint - an endpoint to display page details. It should be without uuid, as later each page tool will be append it's own page uuid
  * @property {boolean} preserveBlank - Whether or not to keep blank Pages when saving editor data
  */
 
 /**
  * @typedef {Object} PageToolData
  * @description Tool's input and output data format
- * @property {number} uuid — Page's uuid.
  * @property {string} title — Page's content. Can include HTML tags: <b><i>
- * @property {boolean} [isCompleted] — flag to show if the tool should be displayed as a link to the created page
+ * @property {boolean} [isPageTitleCompleted] — flag to show if the tool should be displayed as a link to the created page
  */
 class Page {
   /**
@@ -86,7 +84,6 @@ class Page {
      * Tool's data and configurations init
      */
     this._data = {};
-    this._endpoint = config.wikiPageEndpoint || '';
     this._placeholder = config.placeholder ? config.placeholder : Page.DEFAULT_PLACEHOLDER;
     this._preserveBlank = config.preserveBlank !== undefined ? config.preserveBlank : false;
 
@@ -99,9 +96,8 @@ class Page {
    */
   set data(data) {
     this._data = Object.assign({}, {
-      uuid: data.uuid || this._data.uuid,
       title: data.title || this._data.title,
-      isCompleted: data.isCompleted || this._data.isCompleted
+      isPageTitleCompleted: data.isPageTitleCompleted || Boolean(data.title),
     });
   }
 
@@ -126,11 +122,7 @@ class Page {
 
     this._nodes.input.dataset.placeholder = this.api.i18n.t(this._placeholder);
 
-    this._nodes.input.addEventListener('keydown', (event) => {
-      if (event.code === 'Enter' && this._nodes.input.textContent) {
-        this.data = {isCompleted: true};
-      }
-    });
+    this._nodes.input.addEventListener('keydown', this.handleEnterKeeDown.bind(this));
 
     inputHolder.appendChild(this._nodes.input);
 
@@ -143,17 +135,37 @@ class Page {
    */
   prepareLinkPreview() {
     const contentHolder = this.make('div', this._CSS.contentHolder);
-    const pageURL = this._endpoint && this.data.uuid ? `${this._endpoint}/${this.data.uuid}` : '#';
 
     this._nodes.content = this.make('a', [this._CSS.input, this._CSS.contentEl], {
-      target: '_blank',
-      href: pageURL,
+      href: 'javascript:void(0)',
     });
     this._nodes.content.textContent = this.data.title || 'Untitled page';
 
     contentHolder.appendChild(this._nodes.content);
 
+    this._nodes.content.addEventListener('click', this.handleLinkClick.bind(this));
     return contentHolder;
+  }
+
+  /**
+   * "Enter" button keydown event handler. Swap block to display clickable title of the page
+   * @public
+   */
+  handleEnterKeeDown(event) {
+    if (event.code === 'Enter' && this._nodes.input.textContent) {
+      this.data = {isPageTitleCompleted: true};
+    }
+  }
+
+  /**
+   * link click event handler. Emit event to handle the navigation outside of editorjs
+   * @public
+   */
+  handleLinkClick() {
+    const blockAPI = this.api.blocks.getBlockByIndex(this.api.blocks.getCurrentBlockIndex());
+    const pageToolClickEvent = new CustomEvent('pageToolClick', {bubbles: true, detail: {serviceKey: blockAPI.holder.id}});
+
+    blockAPI.holder.dispatchEvent(pageToolClickEvent);
   }
 
   /**
@@ -168,7 +180,7 @@ class Page {
     /**
      * If Tool already has data, render readonly input with a link
      */
-    if (this.data.uuid) {
+    if (this.data.isPageTitleCompleted) {
       this._nodes.contentHolder = this.prepareLinkPreview();
       this._nodes.container.appendChild(this._nodes.contentHolder);
     } else {
@@ -203,10 +215,10 @@ class Page {
    * @public
    */
   save() {
+    const title = this.data.title || this._nodes.input?.textContent || '';
     const payload = {
-      uuid: this.data.uuid,
-      title: this.data.title || this._nodes.input?.textContent || '',
-      isCompleted: !this.data.uuid && this.data.isCompleted,
+      title: title.trim(),
+      isPageTitleCompleted: this.data.isPageTitleCompleted,
     };
 
     Object.keys(payload).forEach(key => {
@@ -216,6 +228,19 @@ class Page {
     })
 
     return payload;
+  }
+
+  /**
+   * Tool is removed from the view
+   * @public
+   */
+  removed() {
+    if (this._nodes.input) {
+      this._nodes.input.removeEventListener('keydown', this.handleEnterKeeDown.bind(this));
+    }
+    if (this._nodes.content) {
+      this._nodes.content.removeEventListener('click', this.handleLinkClick.bind(this));
+    }
   }
 
   /**
